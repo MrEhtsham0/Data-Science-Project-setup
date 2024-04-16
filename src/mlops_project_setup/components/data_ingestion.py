19d5edea85464c0ea6a35af083a3bcf73bc3ad0e
MrEhtsham0 <mr_ehtsham@yahoo.com>
2024-04-16 05:36:49 +0500

    Updating data ingestion

--- src/mlops_project_setup/components/data_ingestion.py
+++ src/mlops_project_setup/components/data_ingestion.py
@@ -5,7 +5,6 @@ from src.mlops_project_setup.logger import logging
 import pandas as pd
 from src.mlops_project_setup.utils import read_sql_data
 from sklearn.model_selection import train_test_split
-
 from dataclasses import dataclass
 @dataclass
 class DataIngestionConfig:
@@ -23,8 +22,8 @@ class DataIngestion:
             os.makedirs(os.path.dirname(self.ingestion.train_datapath),exist_ok=True)
             df.to_csv(self.ingestion.raw_datapath,index=False,header=True)
             train_set,test_set = train_test_split(df,test_size=0.2,random_state=42)
-            df.to_csv(self.ingestion.train_datapath,index=False,header=True)
-            df.to_csv(self.ingestion.test_datapath,index=False,header=True)
+            train_set.to_csv(self.ingestion.train_datapath,index=False,header=True)
+            test_set.to_csv(self.ingestion.test_datapath,index=False,header=True)
 
         except Exception as e:
             raise CustomException(e,sys)
--- src/mlops_project_setup/utils.py
+++ src/mlops_project_setup/utils.py
@@ -7,29 +7,26 @@ from dotenv import load_dotenv
 import pymysql
 
 load_dotenv()
-host=os.getenv("host")
-user=os.getenv("user")
-password=os.getenv("password")
-db=os.getenv('db')
-
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv('db')
 
 
 def read_sql_data():
     logging.info("Reading SQL database started")
     try:
-        mydb=pymysql.connect(
+        mydb = pymysql.connect(
             host=host,
             user=user,
             password=password,
             db=db
         )
-        logging.info("Connection Established",mydb)
-        df=pd.read_sql_query('Select * from dataset',mydb)
+        logging.info("Connection Established: {}".format(mydb))  # Properly format the log message
+        df = pd.read_sql_query('SELECT * FROM dataset', mydb)
         print(df.head())
-
         return df
-
-
-
     except Exception as ex:
-        raise CustomException(ex,sys)
+        # Properly handle exceptions
+        logging.error("Error occurred while reading SQL data: {}".format(ex))
+        raise CustomException(ex, sys)
